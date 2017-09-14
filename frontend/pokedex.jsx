49da53246ddd06c953ee127f54943625799058bb
App Academy Student <student@appacademy.io>
2017-09-13 17:31:33 -0700

    complete part1

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {fetchAllPokemon} from './util/api_util';
-
-import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions';
 import configureStore from './store/store';
-import {selectAllPokemon} from './reducers/selectors';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
   const rootEl = document.getElementById("root");
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
-});
 
-window.selectAllPokemon = selectAllPokemon;
-window.fetchAllPokemon = fetchAllPokemon;
-window.receiveAllPokemon = receiveAllPokemon;
-window.requestAllPokemon = requestAllPokemon;
+  ReactDOM.render(<Root store={store}/>, rootEl);
+});
